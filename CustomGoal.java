7cd19ee2b2f5168dc55e270afdb23cab6cfa7f55
reganYK <regan.yazdankhahkenari@student.tdsb.on.ca>
2025-06-10 23:56:08 -0400

    Update CustomGoal.java

--- CustomGoal.java
+++ CustomGoal.java
@@ -1,8 +1,12 @@
 /**
-*
+* Represents a user-defined custom goal created by the user.
 */
 public class CustomGoal extends Goal{
-	/** ... */
+	/** 
+ 	* Constructs a custom goal with a name and description.
+     	* @param name the name of the goal
+     	* @param description a brief explanation of the goal
+  	*/
 	public CustomGoal(String name, String description){
 		super(name, description);
 	}
