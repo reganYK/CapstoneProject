0fdac6b15d81538d6372ad78932a9c3b16bcd4cd
reganYK <regan.yazdankhahkenari@student.tdsb.on.ca>
2025-06-10 23:54:59 -0400

    Update PredefinedGoal.java

--- PredefinedGoal.java
+++ PredefinedGoal.java
@@ -1,8 +1,12 @@
 /**
-*
+* Represents a predefined goal that the user can select.
 */
 public class PredefinedGoal extends Goal{
-	/** ... */
+	/** 
+ 	* Constructs a predefined goal with a name and description.
+     	* @param name the name of the goal
+     	* @param description a brief explanation of the goal
+  	*/
 	public PredefinedGoal(String name, String description){
 		super(name, description);
 	}
