29d366021fe9b89e4c5944ab590dd9719300f22f
reganYK <regan.yazdankhahkenari@student.tdsb.on.ca>
2025-06-12 23:31:23 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,46 +1,70 @@
 import java.util.*;
 
 /**
-*
+* Represents a user of the EcoPlanner.
+* Stores the user’s goals, logs, and provides streak tracking logic.
 */
 public class User {
+	/** the user's username */
 	private String username;
-	private ArrayList<Goal> goals;
+	/** the list of all goals that belong to the user */
+ 	private ArrayList<Goal> goals;
+	/** the log of completed goals */
 	private ArrayList<GoalLog> logs;
 
-	/** ... */
+	/**
+ 	* Constructs a User with a given username.
+     	* @param username the name of the user
+  	*/
 	public User(String username) {
 		this.username = username;
 		this.goals = new ArrayList<>();
 		this.logs = new ArrayList<>();
 	}
 
-	/** ... */
+	/**
+ 	* Adds a goal to a user's list of goals.
+     	* @param goal that is going to be added
+  	*/
 	public void addGoal(Goal goal) {
 		goals.add(goal);
 	}
 
-	/** ... */
+	/**
+	* Logs the completion of a goal for a specific date.
+        * @param goal the completed goal
+	* @param date it was completed
+  	*/
 	public void logGoalCompleteion(Goal goal, String date) {
 		logs.add(new GoalLog(goal, date));
 	}
 
-	/** ... */
+	/**
+     	* @return list of goals
+  	*/
 	public ArrayList<Goal> getGoals() {
 		return goals;
 	}
 
-	/** ... */
+	/**
+     	* @return list of goal logs
+  	*/
 	public ArrayList<GoalLog> getLogs() {
 		return logs;
 	}
 
-	/** ... */
+	/**
+     	* @return username
+  	*/
 	public String getUsername() {
 		return username;
 	}
 
-	/** ... */
+	/**
+ 	* Counts how many goals were completed on a given date.
+     	* @param date the date to check
+  	* @param number of goals completed on that date
+   	*/
 	public int getGoalsCompletedOnDate(String date) {
 		int count = 0;
 		for(GoalLog log : logs) {
@@ -51,7 +75,11 @@ public class User {
 		return count;
 	}
 
-	/** ... */
+	/**
+ 	* Checks if the user has completed at least 2 goals per day for 30 consecutive days.
+  	* Used to determine if a tree should be planted under the user's name.
+     	* @return true if 30-day streak is reached, false otherwise
+  	*/
 	public boolean hasStreak() {
 		HashMap>String, Integer> dailyCount new HashMap<>();
 		for(GoalLog log : logs) {
